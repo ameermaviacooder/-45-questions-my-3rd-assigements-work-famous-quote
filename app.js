ccb7c855421e461dd008d7f46eab894072f9ad78
ameermaviacooder <160848651+ameermaviacooder@users.noreply.github.com>
2024-03-03 04:03:33 -0800

    3rd assigements famous quote
    ( 45 questions) my 3rd assigements work ,famous quote

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+//1st quote , 
+let quote = "A person who never made a mistake never tried anything new";
+//2nd author
+let author = "Albert Einstein";
+console.log(`${author} once said,"${quote}`);
---
+++ app.ts
@@ -0,0 +1,10 @@
+//1st quote , 
+
+let quote:string= "A person who never made a mistake never tried anything new";
+
+//2nd author
+
+let author: string= "Albert Einstein";
+
+console.log(`${author} once said,"${quote}`);
+
